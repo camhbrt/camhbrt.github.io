3c558e27ed6b11dc8a45a3024f90c7af285c2204
Camille Hebert <camille.hebert22@gmail.com>
2023-09-09 22:36:48 +0200

    new try build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/camhbrt.github.io/', // Remplacez 'mon-projet-vue' par le nom de votre repository GitHub
   plugins: [vue()],
-})
+  // Autres options de configuration ici...
+});
