240ed1b1b6a45f4f34edadb7a81691d7565b4018
Camille Hebert <camille.hebert22@gmail.com>
2023-09-15 15:15:03 +0200

    added new resume

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,9 +4,9 @@
 <template>
     <div  id="home" class=" text-fuchsia-950 flex justify-evenly h-screen">
         <div class="self-center text-6xl">
-            <p class=" font-hachi py-16 text-center">Hi, I'm </p>
-            <p class=" font-hachi py-16 text-center">Camille Hebert !</p>
-            <p class=" font-hachi py-16 text-center">Welcome to my portfolio</p>
+            <p class=" font-hachi leading-loose text-center">Hi, I'm Camille Hébert ! Welcome to my portfolio</p>
+            <!-- <p class=" font-hachi py-16 text-center">Camille Hebert !</p> -->
+            <!-- <p class=" font-hachi py-16 text-center">Welcome to my portfolio</p> -->
         </div>
         <img src="/img/photo.jpg" 
             alt="My Image"
--- src/components/ProjectDetail.vue
+++ src/components/ProjectDetail.vue
@@ -12,22 +12,26 @@
 <template>
     <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
         <div class="bg-rose-100 w-3/4 max-w-screen-md rounded-lg p-6 shadow-lg relative">
-        <button @click="closeDetail" class="absolute top-2 right-4 transform translate-x-2 text-gray-500 hover:text-gray-700">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-            </svg>
-        </button>
-        <img
-            :src=data.img
-            alt="Image"
-            class="h-64 rounded-3xl"
-        />
-        <div class="text-2xl font-semibold mb-4 font-hachi">{{ data.title }}</div>
-        <div class="font-bold mb-4 text-rose-600 uppercase">{{ data.stack }}</div>
-        <div class="mb-4">{{ data.description }}</div>
-        <div class="text-sm font-semibold mb-4">{{ data.date }}</div>
-        <div class="text-sm font-semibold mb-4">durée : {{ data.length }}</div>
-        <div class="text-sm font-semibold mb-4">auteurs : {{ data.authors }}</div>
+            <button @click="closeDetail" class="absolute top-2 right-4 transform translate-x-2 text-gray-500 hover:text-gray-700">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
+            </button>
+            <div class="flex">
+                <div>
+                    <div class="text-4xl font-semibold mb-4 font-hachi text-center">{{ data.title }}</div>
+                    <div class="font-bold mb-4 text-rose-600 uppercase">{{ data.stack }}</div>
+                    <div class="mb-4">{{ data.description }}</div>
+                    <div class="text-sm font-semibold mb-4">{{ data.date }}</div>
+                    <div class="text-sm font-semibold mb-4">durée : {{ data.length }}</div>
+                    <div class="text-sm font-semibold mb-4">auteurs : {{ data.authors }}</div>
+                </div>
+                <img
+                :src=data.img
+                alt="Image"
+                class="h-64 rounded-3xl"
+                />
+            </div>
         </div>
     </div>
 </template>
