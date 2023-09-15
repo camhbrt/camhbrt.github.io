f74d6ac39d0de670a634dcf25fcc9219174c101d
Camille Hebert <camille.hebert22@gmail.com>
2023-09-15 15:49:07 +0200

    cleaned file

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -17,7 +17,6 @@
           <textarea id="message" name="message" placeholder="message content" rows="6" required class="m-4 p-1 rounded-lg border border-fuchsia-950 "></textarea>
         </div>
         <div class="flex justify-end">
-          <!-- <button type="submit" class="py-2 w-20 m-4 font-medium rounded-xl border border-fuchsia-950 text-fuchsia-950 hover:bg-fuchsia-950 hover:text-purple-100 active:tracking-wider">Send</button> -->
           <button type="submit" class="py-2 w-20 m-4 font-medium rounded-xl bg-fuchsia-950 text-purple-100 hover:bg-fuchsia-950/80 active:tracking-wide active:bg-fuchsia-950">Send</button>
         </div>
       </form>
