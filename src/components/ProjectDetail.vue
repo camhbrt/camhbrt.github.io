d73a799a11598932987ff74d5b935b801f755ad4
Camille Hebert <camille.hebert22@gmail.com>
2023-09-18 18:51:37 +0200

    fixed svg / card display problem*

--- src/components/ProjectDetail.vue
+++ src/components/ProjectDetail.vue
@@ -10,7 +10,7 @@
 </script>
 
 <template>
-    <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10">
         <div class="bg-rose-100 lg:w-3/4 max-w-screen-md rounded-lg p-6 shadow-lg relative">
             <button @click="closeDetail" class="absolute top-2 right-4 transform translate-x-2 text-gray-500 hover:text-gray-700">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
