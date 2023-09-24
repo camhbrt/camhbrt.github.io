b2f334124fe7457015ed2458df6c40c7e08e04e1
Camille Hebert <camille.hebert22@gmail.com>
2023-09-24 23:01:21 +0200

    adjusted style

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,78 +4,84 @@
 </script>
 
 <template>
-  <div id="about" class=" text-fuchsia-950 m-4 "> <!--m-20-->
+  <div id="about" class=" text-fuchsia-950 m-4 lg:mx-20"> <!--m-20-->
     <Title :title="'About me'"/>
-    <div class="font-light"><!--flex -->
+    <div class="font-light lg:flex lg:flex-row-reverse"><!--flex -->
       
-      <div id="colonne 2" class="">
-        <p id="biography" class="text-justify">I used to be a middle school music teacher for six years, but I decided to exchange the language of music for the language of programming, and now I have developed a strong enthusiasm for the creative potential offered by software devlopment. <br/><br/>I am currently studying at Ada Tech School, the first feminist and inclusive programming school in Paris, with a pedagogy focused on hands-on practice.</p><!-- text-center lg:w-3/5 lg:pt-20 lg:text-2xl-->
+      <div id="colonne droite" class="lg:basis-3/4 px-20">
+
+        <p id="biography" class="text-justify lg:text-xl lg:pb-10">I used to be a middle school music teacher for six years, but I decided to exchange the language of music for the language of programming, and now I have developed a strong enthusiasm for the creative potential offered by software devlopment. <br/><br/>I am currently studying at Ada Tech School, the first feminist and inclusive programming school in Paris, with a pedagogy focused on hands-on practice.</p><!-- text-center lg:w-3/5 lg:pt-20 lg:text-2xl-->
         
-        <div id="compétences" class="bg-fuchsia-950/10 rounded-3xl p-2 ">
-          
-          <div id="Hard skills" class="py-2">
-            <p class="font-extrabold uppercase text-center">hard skills</p>
-            <div class="flex flex-wrap justify-between">
-              <div 
-                v-for="(skill, index) in data.skills" 
-                :key="index"
-                class="flex flex-col items-center m-2">
-                <p>{{ skill.name }}</p>
-                <p v-html="skill.icon" class="w-12"></p>
-              </div>
+        <div id="ligne 2" class="lg:flex lg:flex-row-reverse lg:justify-between">
+
+          <div id="parcours" class="rounded-3xl bg-fuchsia-950/10 p-4">
+            <div id="formation" class="pb-4">
+              <p class="font-extrabold uppercase text-center">Formation</p>
+              <ul class="pl-4">
+                <li><font-awesome-icon icon="fa-solid fa-computer" class="text-rose-600 pr-2" /> Ada Tech School</li>
+                <li><font-awesome-icon icon="fa-solid fa-music" class="text-rose-600 pr-2" /> Master MEEF Musique</li>
+                <li><font-awesome-icon icon="fa-solid fa-flask-vial" class="text-rose-600 pr-2" /> Baccalauréat scientifique</li>
+              </ul>
+            </div>  
+            <div id="expérience">
+              <p class="font-extrabold uppercase text-center">Expérience</p>
+              <ul class="pl-4 leading-none">
+                <li class="pb-2">
+                  Professeure d'éducation musicale<br/>
+                  <span class="text-rose-600 text-sm font-semibold">2017-2023</span>
+                </li>
+                <li>
+                  Chargée de dépôt<br/>
+                  <span class="text-rose-600 text-sm font-semibold">2013-2016</span >
+                </li>
+              </ul>
             </div>
           </div>
-
-          <div id="Soft skills">
-            <p class="font-extrabold uppercase text-center">soft skills</p>
-            <ul class="text-center m-4">
-              <li><font-awesome-icon icon="fa-solid fa-rotate" class="text-rose-600" /> Adaptabilité</li>
-              <li><font-awesome-icon icon="fa-solid fa-users" class="text-rose-600" /> Intelligence sociale</li>
-              <li><font-awesome-icon icon="fa-solid fa-magnifying-glass" class="text-rose-600" /> Soucis du détail</li>
-              <li><font-awesome-icon icon="fa-solid fa-hand-holding-heart" class="text-rose-600" /> Humilité</li>
-              <li><font-awesome-icon icon="fa-solid fa-bolt" class="text-rose-600" /> Persévérance</li>
+        
+          <div id="coordonnées" class="flex flex-col my-4 leading-loose lg:border lg:rounded-3xl lg:border-fuchsia-950 lg:p-2">
+            <p class="font-extrabold uppercase text-center">coordonnées</p>
+            <ul class="text-center">
+              <li><font-awesome-icon icon="fa-solid fa-location-dot" class="" /> paris, france</li>
+              <li><font-awesome-icon icon="fa-solid fa-at" class="" /> camille.hebert22@gmail.com</li>
+              <li><font-awesome-icon icon="fa-solid fa-phone" class="" /> 06.84.40.81.25</li>
+              <li>
+                <a href="https://github.com/camhbrt" target="_blank" class="hover:text-rose-600"><font-awesome-icon icon="fa-brands fa-github" class="" /> /camhbrt</a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/camille-hebert-dev/" target="_blank" class="hover:text-rose-600"><font-awesome-icon icon="fa-brands fa-linkedin" class="" /> /camille-hebert-dev</a>
+              </li>
             </ul>
           </div>
-
-        </div>
         
-        <div id="coordonnées" class="flex flex-col justify-center items-center my-4">
-          <p><font-awesome-icon icon="fa-solid fa-location-dot" class="" /> paris, france</p>
-          <p><font-awesome-icon icon="fa-solid fa-at" class="" /> camille.hebert22@gmail.com</p>
-          <p><font-awesome-icon icon="fa-solid fa-phone" class="" /> 06.84.40.81.25</p>
-          <a href="https://www.linkedin.com/in/camille-hebert-dev/" target="_blank" class="hover:text-rose-600"><font-awesome-icon icon="fa-brands fa-linkedin" class="" /> /camille-hebert-dev</a>
-          <a href="https://github.com/camhbrt" target="_blank" class="hover:text-rose-600"><font-awesome-icon icon="fa-brands fa-github" class="" /> /camhbrt</a>
-        </div>
-
       </div>
 
-      <div id="colonne 1" class="bg-fuchsia-950/10 rounded-3xl p-2 m-6">
+    </div>
 
-        <div id="formation" class="pb-4">
-          <p class="font-extrabold uppercase text-center">Formation</p>
-          <ul class="pl-4">
-            <li>
-              <font-awesome-icon icon="fa-solid fa-computer" class="text-rose-600 pr-2" />
-              Ada Tech School
-            </li>
-            <li>
-              <font-awesome-icon icon="fa-solid fa-music" class="text-rose-600 pr-2" />
-              Master MEEF Musique
-            </li>
-            <li>
-              <font-awesome-icon icon="fa-solid fa-flask-vial" class="text-rose-600 pr-2" />
-               Baccalauréat scientifique
-              </li>
-          </ul>
+      <div id="compétences" class="lg:basis-1/4 bg-fuchsia-950/10 rounded-3xl p-2 mx-10">
+
+        <div id="Hard skills" class="py-2">
+          <p class="font-extrabold uppercase text-center">hard skills</p>
+          <div class="flex flex-wrap justify-between">
+            <div 
+              v-for="(skill, index) in data.skills" 
+              :key="index"
+              class="flex flex-col items-center m-2">
+              <p>{{ skill.name }}</p>
+              <p v-html="skill.icon" class="w-12"></p>
+            </div>
+          </div>
         </div>
-        
-        <div id="expérience">
-          <p class="font-extrabold uppercase text-center">Expérience</p>
-          <ul class="pl-4 leading-none">
-            <li class="pb-2">Professeure d'éducation musicale <br/><span class="text-rose-600 text-sm font-semibold">2017-2023</span></li>
-            <li>Chargée de dépôt<br/><span class="text-rose-600 text-sm font-semibold">2013-2016</span ></li>
+          
+        <div id="Soft skills">
+          <p class="font-extrabold uppercase text-center">soft skills</p>
+          <ul class="text-center m-4">
+            <li><font-awesome-icon icon="fa-solid fa-rotate" class="text-rose-600" /> Adaptabilité</li>
+            <li><font-awesome-icon icon="fa-solid fa-users" class="text-rose-600" /> Intelligence sociale</li>
+            <li><font-awesome-icon icon="fa-solid fa-magnifying-glass" class="text-rose-600" /> Soucis du détail</li>
+            <li><font-awesome-icon icon="fa-solid fa-hand-holding-heart" class="text-rose-600" /> Humilité</li>
+            <li><font-awesome-icon icon="fa-solid fa-bolt" class="text-rose-600" /> Persévérance</li>
           </ul>
-        </div>
+        </div> 
 
       </div>
       
