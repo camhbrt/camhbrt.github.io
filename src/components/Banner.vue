e1d39a92fcc2122f5d2cfe5d9babe6b7b0cdc114
Camille Hebert <camille.hebert22@gmail.com>
2024-10-10 00:38:00 +0200

    update wording

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -3,6 +3,6 @@
 
 <template>
     <div  class="py-4 sticky w-full top-0 z-10 text-center  text-orange-50 bg-black font-outfit font-normal  ">
-        🚧 REWORK IN PROGRESS (SINCE OCT.2024) 🚧
+        🚧 OCT.2024 : REWORK IN PROGRESS 🚧
     </div>
 </template>
